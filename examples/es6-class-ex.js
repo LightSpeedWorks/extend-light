d9cc68f24cfb84bfd06e10bf3f5686176d25dc9a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-12-01 06:43:25 +0900

    revise

--- examples/es6-class-ex.js
+++ examples/es6-class-ex.js
@@ -1,25 +1,30 @@
 // ES6時代の継承
-class Animal {
-	constructor(name) {
-		this.name = name;
-	}
-	introduce() {
-		console.log('Hello, my name is ' + this.name + '.');
-		this.constructor.introduce();
-	}
-	static introduce() {
-		if ('AIUEO'.indexOf(this.name[0]) >= 0)
-			console.log('I am an ' + this.name + '.');
-		else
-			console.log('I am a ' + this.name + '.');
-	}
-}
+void function () {
+	'use strict';
 
-class Horse extends Animal {}
-var a1 = new Animal('Annie');
-a1.introduce();
-Animal.introduce();
+	class Animal {
+		constructor(name) {
+			this.name = name;
+		}
+		introduce() {
+			console.log('Hello, my name is ' + this.name + '.');
+			this.constructor.introduce();
+		}
+		static introduce() {
+			console.log('I am ' +
+				('AIUEO'.indexOf(this.name[0]) >= 0 ? 'an ' : 'a ') +
+				this.name + '.');
+		}
+	}
 
-var h1 = new Horse('Deep Impact');
-h1.introduce();
-Horse.introduce();
+	class Horse extends Animal {}
+
+	var a1 = new Animal('Annie');
+	a1.introduce();
+	Animal.introduce();
+
+	var h1 = new Horse('Deep Impact');
+	h1.introduce();
+	Horse.introduce();
+
+}();
